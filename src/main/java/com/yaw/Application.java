0cb57fd458e0c979105c447735afc4a88e77944d
yawpei <yawpei@126.com>
2016-12-21 17:40:40 +0800

    添加todo，不打印banner未实现

--- src/main/java/com/yaw/Application.java
+++ src/main/java/com/yaw/Application.java
@@ -2,7 +2,6 @@ package com.yaw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * Created by yawpei on 2016/12/21.
